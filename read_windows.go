efe8fdc0c19a321ca2d043dd471b8f71f8fa0b3f
Aki Kareha <aki@kareha.org>
2026-07-11 21:28:03 +0900

    Handle raw key event

--- read_windows.go
+++ read_windows.go
@@ -3,15 +3,17 @@
 package termi
 
 import (
-	"fmt"
 	"os"
-	"sync/atomic"
-	"syscall"
+	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
 
-var stopping = atomic.Bool{}
+func isWindowsTerminal() bool {
+	return os.Getenv("WT_SESSION") != ""
+}
+
+var windowsTerminal = isWindowsTerminal()
 
 var stopEvent windows.Handle
 
@@ -20,29 +22,51 @@ var readThread windows.Handle
 var (
 	kernel32 = windows.NewLazySystemDLL("kernel32.dll")
 
-	procCancelSynchronousIo = kernel32.NewProc("CancelSynchronousIo")
+	procReadConsoleInputW = kernel32.NewProc("ReadConsoleInputW")
 )
 
-func CancelSynchronousIo(h windows.Handle) error {
-	r1, _, e1 := procCancelSynchronousIo.Call(uintptr(h))
+type KeyEventRecord struct {
+	KeyDown         int32
+	RepeatCount     uint16
+	VirtualKeyCode  uint16
+	VirtualScanCode uint16
+	UnicodeChar     uint16
+	ControlKeyState uint32
+}
+
+const EventTypeKey = 0x0001
+
+type InputRecord struct {
+	EventType uint16
+	_         uint16
+	KeyEvent  KeyEventRecord
+}
+
+func ReadConsoleInput(
+	h windows.Handle,
+	rec *InputRecord,
+	length uint32,
+	read *uint32,
+) error {
+	r1, _, e := procReadConsoleInputW.Call(
+		uintptr(h),
+		uintptr(unsafe.Pointer(rec)),
+		uintptr(length),
+		uintptr(unsafe.Pointer(read)),
+	)
 	if r1 == 0 {
-		if e1 != windows.ERROR_SUCCESS {
-			return e1
-		}
-		return syscall.EINVAL
+		return e
 	}
 	return nil
 }
 
 func initRead() error {
-	stopping.Store(false)
 	var err error
 	stopEvent, err = windows.CreateEvent(nil, 1, 0, nil)
 	return err
 }
 
 func stopRead() error {
-	stopping.Store(true)
 	return windows.SetEvent(stopEvent)
 }
 
@@ -52,16 +76,38 @@ func finishRead() error {
 
 var readBuf [1]byte
 
-func read() (byte, error) {
-	for {
-		n, err := os.Stdin.Read(readBuf[:])
-		if err != nil {
-			return 0, err
+func read() (byte, bool, error) {
+	if windowsTerminal {
+		for {
+			n, err := os.Stdin.Read(readBuf[:])
+			if err != nil {
+				return 0, false, err
+			}
+			if n == 1 {
+				b := readBuf[0]
+				checkEscape(b)
+				return b, true, nil
+			}
 		}
-		if n == 1 {
-			b := readBuf[0]
-			checkEscape(b)
-			return b, nil
+	} else {
+		for {
+			var rec InputRecord
+			var n uint32
+			err := ReadConsoleInput(windows.Handle(os.Stdin.Fd()), &rec, 1, &n)
+			if err != nil {
+				return 0, false, err
+			}
+			if n == 1 {
+				if rec.EventType != EventTypeKey {
+					return 0, false, nil
+				}
+				if rec.KeyEvent.KeyDown == 0 {
+					return 0, false, nil
+				}
+				b := byte(rec.KeyEvent.UnicodeChar)
+				checkEscape(b)
+				return b, true, nil
+			}
 		}
 	}
 }
@@ -84,16 +130,14 @@ func spawnReader() {
 			}
 			switch n {
 			case windows.WAIT_OBJECT_0:
-				fmt.Fprint(os.Stderr, "(stopEvent)") // XXX
 				break loop
 			case windows.WAIT_OBJECT_0 + 1:
-				fmt.Fprint(os.Stderr, "(input)") // XXX
-				if stopping.Load() {
-					break loop
-				}
-				b, err := read()
+				b, ok, err := read()
 				if err != nil {
 					break loop
+				}
+				if !ok {
+					continue
 				}
 				readCh <- b
 			}
