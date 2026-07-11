3ce8a94b189dd9390ee80d73fa3f0d9cf4200508
Aki Kareha <aki@kareha.org>
2026-07-11 20:34:26 +0900

    Swap event order

--- read_windows.go
+++ read_windows.go
@@ -3,19 +3,15 @@
 package termi
 
 import (
+	"fmt"
 	"os"
-	"runtime"
+	"sync/atomic"
 	"syscall"
-	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
 
-func isWindowsTerminal() bool {
-	return os.Getenv("WT_SESSION") != ""
-}
-
-var windowsTerminal = isWindowsTerminal()
+var stopping = atomic.Bool{}
 
 var stopEvent windows.Handle
 
@@ -25,7 +21,6 @@ var (
 	kernel32 = windows.NewLazySystemDLL("kernel32.dll")
 
 	procCancelSynchronousIo = kernel32.NewProc("CancelSynchronousIo")
-	procReadConsoleInputW   = kernel32.NewProc("ReadConsoleInputW")
 )
 
 func CancelSynchronousIo(h windows.Handle) error {
@@ -39,101 +34,45 @@ func CancelSynchronousIo(h windows.Handle) error {
 	return nil
 }
 
-type KeyEventRecord struct {
-	KeyDown         int32
-	RepeatCount     uint16
-	VirtualKeyCode  uint16
-	VirtualScanCode uint16
-	UnicodeChar     uint16
-	ControlKeyState uint32
-}
-
-type InputRecord struct {
-	EventType uint16
-	_         uint16 // alignment
-	KeyEvent  KeyEventRecord
-}
-
-func ReadConsoleInput(
-	h windows.Handle,
-	rec *InputRecord,
-	length uint32,
-	read *uint32,
-) error {
-	r1, _, e := procReadConsoleInputW.Call(
-		uintptr(h),
-		uintptr(unsafe.Pointer(rec)),
-		uintptr(length),
-		uintptr(unsafe.Pointer(read)),
-	)
-	if r1 == 0 {
-		return e
-	}
-	return nil
-}
-
 func initRead() error {
-	if windowsTerminal {
-		var err error
-		stopEvent, err = windows.CreateEvent(nil, 1, 0, nil)
-		return err
-	} else {
-		return nil
-	}
+	stopping.Store(false)
+	var err error
+	stopEvent, err = windows.CreateEvent(nil, 1, 0, nil)
+	return err
 }
 
 func stopRead() error {
-	if windowsTerminal {
-		return windows.SetEvent(stopEvent)
-	} else {
-		return CancelSynchronousIo(readThread)
-	}
+	stopping.Store(true)
+	return windows.SetEvent(stopEvent)
 }
 
 func finishRead() error {
-	if windowsTerminal {
-		return windows.CloseHandle(stopEvent)
-	} else {
-		return nil
-	}
+	return windows.CloseHandle(stopEvent)
 }
 
 var readBuf [1]byte
 
 func read() (byte, error) {
 	for {
-		/*
-			n, err := os.Stdin.Read(readBuf[:])
-			if err != nil {
-				return 0, err
-			}
-			if n == 1 {
-				b := readBuf[0]
-				checkEscape(b)
-				return b, nil
-			}
-		*/
-		var rec InputRecord
-		var n uint32
-		err := ReadConsoleInput(windows.Handle(os.Stdin.Fd()), &rec, 1, &n)
+		n, err := os.Stdin.Read(readBuf[:])
 		if err != nil {
 			return 0, err
 		}
 		if n == 1 {
-			b := byte(rec.KeyEvent.UnicodeChar)
+			b := readBuf[0]
 			checkEscape(b)
 			return b, nil
 		}
 	}
 }
 
-func spawnReaderForWindowsTerminal() {
+func spawnReader() {
 	keyWG.Add(1)
 	go func() {
 		defer keyWG.Done()
 
 		input := windows.Handle(os.Stdin.Fd())
-		handles := []windows.Handle{input, stopEvent}
+		handles := []windows.Handle{stopEvent, input}
 
 	loop:
 		for {
@@ -145,59 +84,21 @@ func spawnReaderForWindowsTerminal() {
 			}
 			switch n {
 			case windows.WAIT_OBJECT_0:
+				fmt.Fprint(os.Stderr, "(stopEvent)") // XXX
+				break loop
+			case windows.WAIT_OBJECT_0 + 1:
+				fmt.Fprint(os.Stderr, "(input)") // XXX
+				if stopping.Load() {
+					break loop
+				}
 				b, err := read()
 				if err != nil {
 					break loop
 				}
 				readCh <- b
-			case windows.WAIT_OBJECT_0 + 1:
-				break loop
 			}
 		}
 		close(readDone)
 		close(readCh)
 	}()
-}
-
-func spawnReaderForOtherTerminals() {
-	keyWG.Add(1)
-	go func() {
-		defer keyWG.Done()
-
-		runtime.LockOSThread()
-		defer runtime.UnlockOSThread()
-
-		h, err := windows.OpenThread(
-			windows.THREAD_TERMINATE,
-			false,
-			windows.GetCurrentThreadId(),
-		)
-		if err != nil {
-			// XXX inform error
-			close(readDone)
-			close(readCh)
-			return
-		}
-		defer windows.CloseHandle(h)
-
-		readThread = h
-
-		for {
-			b, err := read()
-			if err != nil {
-				break
-			}
-			readCh <- b
-		}
-		close(readDone)
-		close(readCh)
-	}()
-}
-
-func spawnReader() {
-	if windowsTerminal {
-		spawnReaderForWindowsTerminal()
-	} else {
-		spawnReaderForOtherTerminals()
-	}
 }
