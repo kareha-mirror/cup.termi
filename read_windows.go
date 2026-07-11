cf0e2f29a1f5119777f5916b6d20411ae62448e6
Aki Kareha <aki@kareha.org>
2026-07-11 19:02:35 +0900

    Use windows.ReadFile directly

--- read_windows.go
+++ read_windows.go
@@ -3,7 +3,7 @@
 package termi
 
 import (
-	"errors"
+	"fmt"
 	"os"
 	"runtime"
 	"syscall"
@@ -17,32 +17,11 @@ func isWindowsTerminal() bool {
 
 var windowsTerminal = isWindowsTerminal()
 
-var stdin *os.File
-
 var stopEvent windows.Handle
 
 var readThread windows.Handle
 
-func duplicateHandle(h windows.Handle) (windows.Handle, error) {
-	proc := windows.CurrentProcess()
-
-	var dup windows.Handle
-	err := windows.DuplicateHandle(
-		proc, h, proc, &dup, 0, false, windows.DUPLICATE_SAME_ACCESS,
-	)
-	if err != nil {
-		return 0, err
-	}
-	return dup, nil
-}
-
 func initRead() error {
-	h := windows.Handle(os.Stdin.Fd())
-	dup, err := duplicateHandle(h)
-	if err != nil {
-		return err
-	}
-	stdin = os.NewFile(uintptr(dup), "stdin-dup")
 	if windowsTerminal {
 		var err error
 		stopEvent, err = windows.CreateEvent(nil, 1, 0, nil)
@@ -73,28 +52,27 @@ func stopRead() error {
 	if windowsTerminal {
 		return windows.SetEvent(stopEvent)
 	} else {
-		//return CancelSynchronousIo(readThread)
-		err := stdin.Close()
-		return errors.Join(err, CancelSynchronousIo(readThread))
+		return CancelSynchronousIo(readThread)
 	}
 }
 
 func finishRead() error {
-	var err error
 	if windowsTerminal {
-		err = windows.CloseHandle(stopEvent)
+		return windows.CloseHandle(stopEvent)
 	} else {
-		//err = nil
 		return nil
 	}
-	return errors.Join(err, stdin.Close())
 }
 
 var readBuf [1]byte
 
 func read() (byte, error) {
 	for {
-		n, err := stdin.Read(readBuf[:])
+		var n uint32
+		err := windows.ReadFile(
+			windows.Handle(os.Stdin.Fd()), readBuf[:], &n, nil,
+		)
+		fmt.Fprintf(os.Stderr, "(%d,%v)", n, err) // XXX debug
 		if err != nil {
 			return 0, err
 		}
@@ -111,7 +89,7 @@ func spawnReaderForWindowsTerminal() {
 	go func() {
 		defer keyWG.Done()
 
-		input := windows.Handle(stdin.Fd())
+		input := windows.Handle(os.Stdin.Fd())
 		handles := []windows.Handle{input, stopEvent}
 
 	loop:
